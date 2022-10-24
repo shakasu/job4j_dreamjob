c014136369da544e0544f4baed80386411cfa173
Karen Shakhnazaryan <kshakhnazaryan@innodata.ru>
2022-10-24 23:14:34 +0300

    4. Thymeleaf, Циклы. [#504841]

---
+++ job4j_dreamjob/src/main/java/ru/job4j/dreamjob/model/Post.java
@@ -0,0 +1,71 @@
+package ru.job4j.dreamjob.model;
+
+import java.util.Objects;
+
+public class Post {
+    private int id;
+    private String name;
+
+    private String description;
+
+    private String created;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public Post() {
+    }
+
+    public Post(int id, String name, String description, String created) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id == post.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ job4j_dreamjob/src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -0,0 +1,28 @@
+package ru.job4j.dreamjob.store;
+
+import ru.job4j.dreamjob.model.Post;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class PostStore {
+
+    private static final PostStore INST = new PostStore();
+
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    private PostStore() {
+        posts.put(1, new Post(1, "Junior Java Job", "Solving easy tasks", "12.03.2021"));
+        posts.put(2, new Post(2, "Middle Java Job", "Solving medium tasks", "12.03.2021"));
+        posts.put(3, new Post(3, "Senior Java Job","Solving hard tasks", "12.03.2021"));
+    }
+
+    public static PostStore instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+}
