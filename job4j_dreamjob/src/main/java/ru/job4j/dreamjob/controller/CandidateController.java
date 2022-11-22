27ecc9d89597fcf07d1fe81df9c86d29be479803
Karen Shakhnazaryan <kshakhnazaryan@innodata.ru>
2022-11-22 23:49:12 +0300

    Формы. Загрузка файла на сервер. [#504855 #358953]

--- job4j_dreamjob/src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ job4j_dreamjob/src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -60,7 +60,10 @@ public class CandidateController {
     }
 
     @PostMapping("/updateCandidate")
-    public String updateCandidate(@ModelAttribute Candidate candidate) {
+    public String updateCandidate(@ModelAttribute Candidate candidate,
+                                  @RequestParam("file") MultipartFile file,
+                                  HttpServletRequest request) throws IOException {
+        candidate.setPhoto(file.getBytes());
         service.update(candidate);
         return "redirect:/candidates";
     }
